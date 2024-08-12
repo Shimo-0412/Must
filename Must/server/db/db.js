eb459f8ba40baf7a89a645f3d58046d03b7e5a02
Shimo_0412 <shimo_0412@outlook.jp>
2024-08-12 21:08:57 +0900

    Add files via upload

---
+++ Must/server/db/db.js
@@ -0,0 +1,19 @@
+// db.js
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'your_username',
+  password: 'your_password',
+  database: 'your_database'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  console.log('Connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
